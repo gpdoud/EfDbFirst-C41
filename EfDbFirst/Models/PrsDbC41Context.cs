83e1b14189a88dc71060e7091ee010bac20e5f73
Greg Doud <gpdoud@gmail.com>
2023-08-28 09:54:25 -0400

    Add project files.

---
+++ EfDbFirst/Models/PrsDbC41Context.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfDbFirst.Models;
+
+public partial class PrsDbC41Context : DbContext
+{
+    public PrsDbC41Context()
+    {
+    }
+
+    public PrsDbC41Context(DbContextOptions<PrsDbC41Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Request> Requests { get; set; }
+
+    public virtual DbSet<Requestline> Requestlines { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<Vendor> Vendors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder
+            .UseSqlServer("server=localhost\\sqlexpress;database=PrsDbC41;trusted_connection=true;trustServerCertificate=true;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07AC469F13");
+
+            entity.HasIndex(e => e.PartNbr, "UQ__Products__DAFC0C1E13E69511").IsUnique();
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.PartNbr)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.PhotoPath)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Price)
+                .HasDefaultValueSql("((10))")
+                .HasColumnType("decimal(11, 2)");
+            entity.Property(e => e.Unit)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('Each')");
+
+            entity.HasOne(d => d.Vendor).WithMany(p => p.Products)
+                .HasForeignKey(d => d.VendorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Products__Vendor__4222D4EF");
+        });
+
+        modelBuilder.Entity<Request>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Requests__3214EC0718FCAA0F");
+
+            entity.Property(e => e.DeliveryMode)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('Pickup')");
+            entity.Property(e => e.Description)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Justification)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.RejectionReason)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Status)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('NEW')");
+            entity.Property(e => e.Total).HasColumnType("decimal(11, 2)");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Requests)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Requests__UserId__48CFD27E");
+        });
+
+        modelBuilder.Entity<Requestline>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Requestl__3214EC073AAC75CD");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Requestlines)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Requestli__Produ__4D94879B");
+
+            entity.HasOne(d => d.Request).WithMany(p => p.Requestlines)
+                .HasForeignKey(d => d.RequestId)
+                .HasConstraintName("FK__Requestli__Reque__4CA06362");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07952E1560");
+
+            entity.HasIndex(e => e.Username, "UQ__Users__536C85E46F2B18B6").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(12)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Vendor>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Vendors__3214EC071194C4F0");
+
+            entity.HasIndex(e => e.Code, "UQ__Vendors__A25C5AA7FF5E85AC").IsUnique();
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.City)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Code)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(12)
+                .IsUnicode(false);
+            entity.Property(e => e.State)
+                .HasMaxLength(2)
+                .IsUnicode(false);
+            entity.Property(e => e.Zip)
+                .HasMaxLength(5)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EfDbFirst/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EfDbFirst.Models;
+
+var _context = new PrsDbC41Context();
+
+var users = _context.Users.ToList();
+
+foreach(var user in users) {
+    Console.WriteLine($"{user.Firstname} {user.Lastname}");
+}
